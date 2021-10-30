fb6e1f6c83770834dfeb08da37ebeb7a0ead7f0e
I-polusta <polustaishan2001@gmail.com>
2021-10-31 01:39:31 +0530

    updating designs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Route, Router, Switch } from "react-router";
 import "./App.css";
+import React, { component } from "react";
 import Admin__loginForm from "./components/Admin__loginform";
+import Admin__loginform from "./components/Admin__loginform";
 import Admin__login from "./components/Admin__loginform";
 import Email__verify from "./components/Email__verify";
 import Otp__verify from "./components/Otp__verify";
@@ -8,10 +11,9 @@ import Password__form from "./components/Password__form";
 function App() {
   return (
     <div className="App">
-      {/* <Admin__loginForm /> */}
-      {/* <Email__verify /> */}
+      <Admin__loginForm />
       {/* <Otp__verify /> */}
-      <Password__form />
+      {/* <Password__form /> */}
     </div>
   );
 }
--- src/components/Password__form.js
+++ src/components/Password__form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import image from "../assets/create__pasword.png";
 function Password__form() {
   return (
