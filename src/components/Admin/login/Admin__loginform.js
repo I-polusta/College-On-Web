90e0702b015e0cff392713d2feb6633521979b92
I-polusta <polustaishan2001@gmail.com>
2021-11-01 00:23:52 +0530

    adding validation to one form for testing

--- src/components/Admin/login/Admin__loginform.js
+++ src/components/Admin/login/Admin__loginform.js
@@ -2,8 +2,21 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import image from "../../../assets/admin__login.png";
 import "../../loginform.css";
+import { validEmail, validPassword } from "../../Validation/Validator";
 
 function Admin__loginform() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+  const validate = () => {
+    if (!validEmail.test(email)) {
+      setEmailErr(true);
+    }
+    if (!validPassword.test(password)) {
+      setPwdError(true);
+    }
+  };
   return (
     <div className="container">
       <nav>
@@ -17,27 +30,29 @@ function Admin__loginform() {
             <input
               type="email"
               placeholder="Email Address"
-              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             ></input>
+            {emailErr && <p>Your email is invalid</p>}
             <input
               type="password"
               placeholder="Password"
-              id="password"
-              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             ></input>
+            {pwdError && <p>Your password is invalid</p>}
           </form>
-          <Link to="/">
+          <Link to="resetPassword">
             <a className="forgot" href="#">
               Forgot Password
             </a>
           </Link>
-          <Link to="/admin-dashboard">
-            <button className="btn__admin" type="submit" form="" value="submit">
-              Log In
-            </button>
-          </Link>
+          {/* <Link to="/admin-dashboard"> */}
+          <button className="btn__admin" type="submit" onClick={validate}>
+            Log In
+          </button>
+          {/* </Link> */}
         </div>
-
         <div className="img__container">
           <img src={image} alt="illustration" />
         </div>
